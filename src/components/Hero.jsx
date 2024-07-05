6e9ef25230946ea8d6b7e5993c40c93e5bae1fec
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-05 13:21:56 +0530

    .......

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,24 @@
 import { HERO_CONTENT } from "../constants";
-import Profile_Pic from "../assets/kevinRushProfile.png";
+import Profile_Pic from "../assets/Mypic.png";
 const Hero = () => {
     return (
         <div className="border-b border-neutral-900 pb-4 lg:mb-35">
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pb-12 text-4xl font-thin tracking-tight lg:mt-10
+                        <h1 className="pb-16  text-4xl font-thin tracking-tight lg:mt-15
                         lg:text-7xl">Ananya Sadhukhan</h1>
                         <span className="bg-gradient-to-r from-pink-300 via-slate-500 
                         to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                             Software Developer
                         </span>
-                        <p className="my-2 max-w-xl py-6 font-light tracking-tight">{HERO_CONTENT}</p>
+                        <p className="my-2 max-w-xl py-6  font-light tracking-tight">{HERO_CONTENT}</p>
                     </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center lg:justify-end">
+                        <img className="w-1/2 rounded-full sm:w-1/2 lg:mr-40" src={Profile_Pic} alt="kevin" />
+                </div>
                 </div>
             </div>
         </div>
