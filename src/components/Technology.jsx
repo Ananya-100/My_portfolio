e7613713b02cf35f145e9302de94e52f94b3772e
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-08 00:00:57 +0530

    icon add

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,12 +1,30 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { BsFiletypeSql } from "react-icons/bs";
+import { FaJava } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { SiSpringboot } from "react-icons/si";
 
 const Technology = () => {
     return (
         <div className="border-b border-neutral-800 pb-24">
             <h2 className="my-20 text-center text-4xl">Technologies</h2>
-            <div className="flex flex-wrap items-center justify-center gap4">
+            <div className="flex flex-wrap items-center justify-center gap-4">
                 <div className="rounded-2xl border-4 border-neutral-800 p-4">
-
+                    <RiReactjsLine className="text-7xl text-orange-400"/>
                 </div>
+                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <BsFiletypeSql className="text-7xl text-orange-400" />
+                </div>
+                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaJava className="text-7xl text-orange-400" />
+                </div>
+                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaPython className="text-7xl text-orange-400" />
+                </div>
+                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiSpringboot className="text-7xl text-orange-400" />
+                </div>
+
             </div>
         </div>
     );
