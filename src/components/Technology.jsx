3daab773e4300f6e145edc533c48912d1fc2b530
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-08 00:10:53 +0530

    colou addition

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -10,19 +10,19 @@ const Technology = () => {
             <h2 className="my-20 text-center text-4xl">Technologies</h2>
             <div className="flex flex-wrap items-center justify-center gap-4">
                 <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiReactjsLine className="text-7xl text-orange-400"/>
+                    <RiReactjsLine className="text-7xl text-cyan-400"/>
                 </div>
                 <div className="rounded-2xl border-4 border-neutral-800 p-4">
                     <BsFiletypeSql className="text-7xl text-orange-400" />
                 </div>
                 <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <FaJava className="text-7xl text-orange-400" />
+                    <FaJava className="text-7xl text-red-800" />
                 </div>
                 <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <FaPython className="text-7xl text-orange-400" />
+                    <FaPython className="text-7xl text-sky-400" />
                 </div>
                 <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <SiSpringboot className="text-7xl text-orange-400" />
+                    <SiSpringboot className="text-7xl text-green-600" />
                 </div>
 
             </div>
