cd7815a28e3174a593342c1172fe4a2b5141201e
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-08 00:20:56 +0530

    content of project section

---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { BsFiletypeSql } from "react-icons/bs";
+import { FaJava } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { SiSpringboot } from "react-icons/si";
+
+const Project = () => {
+    return (
+        <div>Project</div>
+    );
+};
+export default Project;
