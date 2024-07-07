94383a6867d882eda2bb09e3c3ff8b2de1bde656
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-08 00:26:37 +0530

    styling text

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,10 @@ import { SiSpringboot } from "react-icons/si";
 
 const Project = () => {
     return (
-        <div className="border-b border-neutral-900 pb-4">Project</div>
+        <div className="border-b border-neutral-900 pb-4">
+            <h2 className="my-20 text-center text-4xl">Project</h2>
+        </div>
+        
     );
 };
 export default Project;
