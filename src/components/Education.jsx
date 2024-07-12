2f09028c07ac0dacd486d40d3b21b84acfcdcc5a
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-12 19:51:04 +0530

    styling

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,15 +6,15 @@ const Education = () => {
         <div className="border-b border-neutral-900 pb-8 ">
             <h2 className="my-20 text-center text-4xl">Education</h2>
             <div className="flex justify-center">
-            <div className="relative border-l border-cyan-900  ">
+            <div className="relative border-l border-fuchsia-200  ">
                 {educationData.map((item, index) => (
                     <div key={index} className="mb-10 ml-4 ">
-                        <div className="absolute -left-2 mt-1.5 w-3 h-3 bg-neutral-900 rounded-full"></div>
+                        <div className="absolute -left-2 mt-1.5 w-3 h-3 bg-fuchsia-200 rounded-full"></div>
                         <div className="pl-8">
-                            <p className="text-base text-gray-500">{item.year}</p>
-                            <h3 className="text-xl font-semibold text-neutral-900">{item.title}</h3>
-                            <h4 className="text-lg font-medium text-neutral-700">{item.institution}</h4>
-                            <p className="mt-2 text-gray-600">{item.description}</p>
+                            <p className="text-base text-fuchsia-400">{item.year}</p>
+                            <h3 className="text-xl font-semibold text-grey-800">{item.title}</h3>
+                            <h4 className="text-md font-medium text-purple-200">{item.institution}</h4>
+                            <p className="mt-2 text-purple-100">{item.description}</p>
                         </div>
                     </div>
                 ))}
