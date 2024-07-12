dbc089bcc42a1104a602b225941b0019baf9d6af
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-12 19:28:26 +0530

    styling in education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,11 +3,12 @@ import { educationData } from "../constants";
 const Education = () => {
     return (
 
-        <div className="border-b border-neutral-900 pb-4">
+        <div className="border-b border-neutral-900 pb-8 ">
             <h2 className="my-20 text-center text-4xl">Education</h2>
-            <div className="relative border-l border-neutral-900">
+            <div className="flex justify-center">
+            <div className="relative border-l border-cyan-900  ">
                 {educationData.map((item, index) => (
-                    <div key={index} className="mb-10 ml-4">
+                    <div key={index} className="mb-10 ml-4 ">
                         <div className="absolute -left-2 mt-1.5 w-3 h-3 bg-neutral-900 rounded-full"></div>
                         <div className="pl-8">
                             <p className="text-base text-gray-500">{item.year}</p>
@@ -17,6 +18,7 @@ const Education = () => {
                         </div>
                     </div>
                 ))}
+            </div>
             </div>
         </div>
     );
