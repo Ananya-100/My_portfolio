4c8ae89e99c92d146b6e463aa75ea048d0b19a04
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-13 21:28:59 +0530

    animation-testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 import { HERO_CONTENT } from "../constants";
 import Profile_Pic from "../assets/Mypic.png";
 import resume from "../constants/Ananya's-Resume.pdf";
-
+import { motion } from "framer-motion";
 const Hero = () => {
     return (
         <div className="border-b border-neutral-900 pb-6 lg:mb-35">
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pb-16  text-4xl font-thin tracking-tight lg:mt-15
-                        lg:text-7xl">Ananya Sadhukhan</h1>
+                        <motion.h1 animate={{ x: 100 }} className="pb-16  text-4xl font-thin tracking-tight lg:mt-15
+                        lg:text-7xl">Ananya Sadhukhan</motion.h1>
                         <span className="bg-gradient-to-r from-pink-300 via-slate-500 
                         to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
                             Software Developer
