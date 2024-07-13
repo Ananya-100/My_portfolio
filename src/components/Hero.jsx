0cb970e7aa0315e5dddcc071871bb736f72e95d1
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-13 21:39:26 +0530

    adding motion in my name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,11 @@ const Hero = () => {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <motion.h1 animate={{ x: 100 }} className="pb-16  text-4xl font-thin tracking-tight lg:mt-15
+                        <motion.h1 
+                        initial = {{x:-100, opacity:0 }}
+                        animate = {{x: 0 , opacity:1  }}
+                        transition={{ duration:0.5, delay: 2}}
+                        className="pb-16  text-4xl font-thin tracking-tight lg:mt-15
                         lg:text-7xl">Ananya Sadhukhan</motion.h1>
                         <span className="bg-gradient-to-r from-pink-300 via-slate-500 
                         to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
