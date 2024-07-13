58aaf8c5d23a4d372710b721ac8dccdcb3444a96
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-13 20:38:17 +0530

    project section changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,19 @@
-import { RiReactjsLine } from "react-icons/ri";
-import { BsFiletypeSql } from "react-icons/bs";
-import { FaJava } from "react-icons/fa";
-import { FaPython } from "react-icons/fa";
-import { SiSpringboot } from "react-icons/si";
+
 import { PROJECTS } from "../constants";
 
 const Project = () => {
     return (
         <div className="border-b border-neutral-900 pb-4">
             <h2 className="my-20 text-center text-4xl">Projects</h2>
-            
+            <div>
+                {PROJECTS.map(project,inedex) = (
+                    <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+                        <div></div>
+                    </div>
+                )
+                
+                }
+            </div>
         </div>
         
     );
