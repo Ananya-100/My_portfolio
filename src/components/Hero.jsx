b40dea31203488293bd48317ccfc938aa3103b7b
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-14 21:38:18 +0530

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,18 +32,21 @@ const Hero = () => {
                             Software Developer
                         </motion.span>
                         <motion.p 
-                            variants={container(0)}
+                            variants={container(1.0)}
                             initial="hidden"
                             animate="visible"
                         className="my-2 max-w-xl py-6  font-light tracking-tight">{HERO_CONTENT}</motion.p>
                         
-                        <button 
+                        <motion.button 
+                            variants={container(1.5)}
+                            initial="hidden"
+                            animate="visible"
                         className="sm:mb-20 text-white bg-gradient-to-r from-purple-500 to-pink-500 
                 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 
                 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center
                  mb-8 ">
                             <a href={resume} download='resume'>Download</a>
-                        </button>
+                        </motion.button>
                         
                     </div>
                 </div>
