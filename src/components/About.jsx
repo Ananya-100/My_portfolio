cb0d539b723e8756198f9b9ce491731fbaf6400b
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-14 21:56:13 +0530

    image-animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,11 +10,15 @@ const About = () => {
                 <span className="text-neutral-500">Me</span>
             </h1>
             <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2 lg:p-8">
+                <motion.div 
+                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0,c:-100}}
+                transition={{duration:0.5}}
+                className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
                         <img className="rounded-2xl w-4/6 " src={About_Pic} alt="about" />
                     </div>
-                </div>
+                </motion.div>
                 <div className="w-full lg:w-2/5">
                     <div className="flex justify-center lg:justify-start">
                         <p className="my-2 max-w-xl py-12">{ABOUT_TEXT}</p>
