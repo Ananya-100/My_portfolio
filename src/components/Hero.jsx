cca0590f4cc4a5121baa455f77f033f285c528ce
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-14 21:46:25 +0530

    image animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -55,7 +55,7 @@ const Hero = () => {
                         <motion.img 
                         initial={{x:100, opacity:0}}
                         animate={{x:0, opacity:1}}
-                        // transition={{ duration:1, delay:1.2}}
+                        transition={{ duration:1, delay:1.2}}
                         className="w-1/2 rounded-full sm:w-1/2 sm:mr-10  lg:mr-30" src={Profile_Pic} alt="kevin" />
                     </div>
                 </div>
