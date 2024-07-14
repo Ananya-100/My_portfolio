8b326bb13c005aca94981338e7608c8023ea6514
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-14 22:27:04 +0530

    button animation

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -24,24 +24,48 @@ const Technology = () => {
         <div className="border-b border-neutral-800 pb-24">
             <h2 className="my-20 text-center text-4xl">Technologies</h2>
             <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiReactjsLine className="text-6xl text-cyan-400"/>
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <BsFiletypeSql className="text-6xl text-orange-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div 
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaJava className="text-6xl text-red-800" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaPython className="text-6xl text-sky-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiSpringboot className="text-6xl text-green-600" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3.5)}
+                    initial="initial"
+                    animate="animate"
+                className="rounded-2xl border-4 border-neutral-800 p-4">
                     <SiPandas className="text-6xl text-blue-600" />
-                </div>
+                </motion.div>
 
             </div>
         </div>
