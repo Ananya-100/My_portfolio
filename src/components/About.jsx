1e36fcb2cd374eeb690082deef3f1ba483cffc2a
Ananya-100 <ananyasadhukhan1803@gmail.com>
2024-07-19 20:56:58 +0530

    about section animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,19 @@ const About = () => {
     return (
         
         <div className="border-b border-neutral-900 pb-4">
-            <h1 className="my-20 text-center text-4xl">
+            <motion.h1
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+            className="my-20 text-center text-4xl">
                 About
                 <span className="text-neutral-500">Me</span>
-            </h1>
+            </motion.h1>
             <div className="flex flex-wrap">
                 <motion.div 
                 whileInView={{opacity:1, x:0}}
                 initial={{opacity:0,x:-100}}
-                transition={{duration:0.5}}
+                transition={{duration:1}}
                 className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
                         <img className="rounded-2xl w-4/6 " src={About_Pic} alt="about" />
@@ -22,7 +26,7 @@ const About = () => {
                 <motion.div
                     whileInView={{ opacity: 1, x: 0 }}
                     initial={{ opacity: 0, x: 100 }}
-                    transition={{ duration: 0.5 }}
+                    transition={{ duration: 1 }}
                 className="w-full lg:w-2/5">
                     <div className="flex justify-center lg:justify-start">
                         <p className="my-2 max-w-xl py-12">{ABOUT_TEXT}</p>
